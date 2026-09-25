561c26d1128044a34b284d321e0f93022b5fda43
zjavierf <120588365+zjavierf@users.noreply.github.com>
2026-09-25 21:19:46 +0100

    Update Results.cs

--- Features/MenuTweaks/Results.cs
+++ Features/MenuTweaks/Results.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using HarmonyLib;
 using SpeecilTweaks.Configuration;
 using UnityEngine;
@@ -8,24 +8,79 @@ namespace SpeecilTweaks.Features.MenuTweaks
     [HarmonyPatch(typeof(ResultsViewController), "SetDataToUI")]
     public static class ResultsViewControllerSetDataToUI
     {
-        static void Postfix(ref GameObject ____clearedBannerGo, ref GameObject ____failedBannerGo)
+        static void Postfix(ResultsViewController __instance)
         {
-            if (____clearedBannerGo != null)
+            if (__instance == null) return;
+            var transform = __instance.transform;
+
+            var clearedBanner = FindRecursive(transform, "ClearedBanner") ?? FindRecursive(transform, "ClearBanner") ?? FindRecursive(transform, "ResultBanner");
+            var failedBanner = FindRecursive(transform, "FailedBanner") ?? FindRecursive(transform, "FailBanner");
+
+            if (clearedBanner != null)
             {
-                ____clearedBannerGo.GetComponentInChildren<HMUI.CurvedTextMeshPro>()?.SetText(PluginConfig.Instance.Text.ResultText);
-                var img = ____clearedBannerGo.GetComponentsInChildren<HMUI.ImageView>().FirstOrDefault();
-                if (img != null) img.color = new Color32((byte)PluginConfig.Instance.Colors.PassBgR, (byte)PluginConfig.Instance.Colors.PassBgG, (byte)PluginConfig.Instance.Colors.PassBgB, 255);
+                var text = clearedBanner.GetComponentInChildren<HMUI.CurvedTextMeshPro>(true);
+                if (text != null)
+                {
+                    text.SetText(PluginConfig.Instance.Text.ResultText);
+                    RemoveLocalizeComponents(clearedBanner.gameObject);
+                }
+
+                var img = clearedBanner.GetComponentsInChildren<HMUI.ImageView>().FirstOrDefault();
+                if (img != null)
+                {
+                    img.color = new Color32(
+                        (byte)PluginConfig.Instance.Colors.PassBgR, 
+                        (byte)PluginConfig.Instance.Colors.PassBgG, 
+                        (byte)PluginConfig.Instance.Colors.PassBgB, 
+                        255
+                    );
+                }
             }
 
-            if (____failedBannerGo != null)
+            if (failedBanner != null)
             {
-                ____failedBannerGo.GetComponentInChildren<HMUI.CurvedTextMeshPro>()?.SetText(PluginConfig.Instance.Text.ResultFailText);
-                var imgFail = ____failedBannerGo.GetComponentsInChildren<HMUI.ImageView>().FirstOrDefault();
-                if (imgFail != null) imgFail.color = new Color32((byte)PluginConfig.Instance.Colors.FailBgR, (byte)PluginConfig.Instance.Colors.FailBgG, (byte)PluginConfig.Instance.Colors.FailBgB, 255);
-                
-                var textFail = ____failedBannerGo.GetComponentsInChildren<HMUI.CurvedTextMeshPro>().FirstOrDefault();
-                if (textFail != null) textFail.color = Color.white;
+                var textFail = failedBanner.GetComponentInChildren<HMUI.CurvedTextMeshPro>(true);
+                if (textFail != null)
+                {
+                    textFail.SetText(PluginConfig.Instance.Text.ResultFailText);
+                    textFail.color = Color.white;
+                    RemoveLocalizeComponents(failedBanner.gameObject);
+                }
+
+                var imgFail = failedBanner.GetComponentsInChildren<HMUI.ImageView>().FirstOrDefault();
+                if (imgFail != null)
+                {
+                    imgFail.color = new Color32(
+                        (byte)PluginConfig.Instance.Colors.FailBgR, 
+                        (byte)PluginConfig.Instance.Colors.FailBgG, 
+                        (byte)PluginConfig.Instance.Colors.FailBgB, 
+                        255
+                    );
+                }
             }
+        }
+
+        private static void RemoveLocalizeComponents(GameObject obj)
+        {
+            foreach (var comp in obj.GetComponentsInChildren<Component>(true))
+            {
+                if (comp != null && comp.GetType().Name.Contains("Localize"))
+                {
+                    Object.Destroy(comp);
+                }
+            }
+        }
+
+        private static Transform? FindRecursive(Transform parent, string name)
+        {
+            foreach (Transform child in parent)
+            {
+                if (child == null) continue;
+                if (child.name.ToLower().Contains(name.ToLower())) return child;
+                var result = FindRecursive(child, name);
+                if (result != null) return result;
+            }
+            return null;
         }
     }
 }
